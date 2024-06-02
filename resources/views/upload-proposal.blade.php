04dbbd6840136a1aff5dae4784f09fee9112e851
Dimas Prasetio <dimas.prasetio@neuronworks.co.id>
2024-06-02 12:21:55 +0700

    update

--- resources/views/upload-proposal.blade.php
+++ resources/views/upload-proposal.blade.php
@@ -38,7 +38,15 @@
                                             Tidak ada file
                                         @endif
                                     </td>
-                                    <td></td>
+                                    <td>
+                                        @if ($proker->proposal && $proker->proposal->pengesahan)
+                                            <a href="{{ asset('pengesahan/' . $proker->proposal->pengesahan) }}" target="_blank">
+                                                {{ $proker->proposal->file_proposal }}
+                                            </a>
+                                        @else
+                                            Tidak ada file
+                                        @endif
+                                    </td>
                                     <td>{{ $proker->proposal ? $proker->proposal->status : 'Pending' }}</td>
                                     <td>{{ $proker->proposal ? $proker->proposal->catatan : 'Tidak Ada Catatan' }}</td>
                                     <td>
