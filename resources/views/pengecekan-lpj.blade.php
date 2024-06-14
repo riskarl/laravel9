8f74465fbc44d38498d1652179f5a358420752ce
Dimas Prasetio <dimas.prasetio@neuronworks.co.id>
2024-06-14 21:24:29 +0700

    update

--- resources/views/pengecekan-lpj.blade.php
+++ resources/views/pengecekan-lpj.blade.php
@@ -27,8 +27,6 @@
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
           <h6 class="m-0 font-weight-bold text-primary">Pengecekan Laporan Pertanggungjawaban</h6>
         </div>
-
-        @endif
         <div class="table-responsive">
           <table class="table align-items-center table-flush">
             <thead class="thead-light">
