0993953158fb4145f1feaf9e83504ab8088eabb3
Dimas Prasetio <dimas.prasetio@neuronworks.co.id>
2024-06-17 09:55:47 +0700

    update

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -68,25 +68,34 @@ class UploadController extends Controller
         if (empty($proker->ttd_ketupel)) {
             return redirect()->back()->with('error', 'TTD Ketupel tidak lengkap');
         }
-    
+
+        $codeJabatan = 6;
+        $status_flow = 0;
         $namaOrganisasi = $proker->organisasi->nama_organisasi;
-        $proposalId = $proposal->id;
-        
-        $user = $this->processStatusFlow($proposalId, $jabatanId, $organisasi, $jabatan, $namaOrganisasi);
-    
-        if ($user) {
-            $result = $this->sendNotificationEmail($user);
-    
-            if ($result) {
-                Session::flash('success', 'Email has been sent.');
-            } else {
-                Session::flash('error', 'Failed to sent the email.');
-                return redirect()->back();
-            }
+
+        if ($codeJabatan !== null) {
+            $user = User::join('jabatan', 'users.jabatan_id', '=', 'jabatan.jabatan_id')
+                ->where('jabatan.code_jabatan', $codeJabatan)
+                ->when($status_flow == 2, function($query) use ($namaOrganisasi) {
+                    return $query->whereRaw('LOWER(users.organization) = ?', [strtolower($namaOrganisasi)]);
+                })
+                ->when($status_flow == 3, function($query) {
+                    return $query->whereRaw('LOWER(users.organization) LIKE ?', ['%bem%']);
+                })
+                ->when($status_flow == 4, function($query) {
+                    return $query->whereRaw('LOWER(users.organization) LIKE ?', ['%bpm%']);
+                })
+                ->select('users.email', 'users.name')
+                ->first();
+
+                $sendEmail = $this->sendNotificationEmail($user);
+
+                if($sendEmail){
+                    return redirect()->back()->with('success', 'File Proposal berhasil diupload!');
+                }else{
+                    return redirect()->back()->with('error', 'gagal kirim email!');   
+                }
         }
-
-
-        return redirect()->back()->with('success', 'File Proposal berhasil diupload!');
     }
 
     public function uploadrab(Request $request, $id)
@@ -118,52 +127,7 @@ class UploadController extends Controller
 
         return redirect()->back()->with('success', 'File RAB berhasil diupload!');
     }
-
-    private function processStatusFlow($proposalId, $jabatanId, $organisasi, $jabatan, $namaOrganisasi)
-    {
-        $mappingCheck = new MappingCheck();
-        $signatures = $mappingCheck->updateStatusFlow($proposalId, $jabatanId, $organisasi, $jabatan);
-        $status_flow = $signatures['status_flow'] == 0 ? $signatures['status_flow'] + 2 : $signatures['status_flow'] + 1;
-        if ($signatures !== false) {
-            $signatures = $this->filterTtdList($signatures['ttdList'], $jabatanId, $organisasi);
-        }
-
-        $status_code_mapping = [
-            0 => 6, // SEKRETARIS
-            1 => 6, // REVISI
-            2 => stripos($namaOrganisasi, 'UKM') !== false ? 5 : 5, // KETUA UKM atau KETUA HIMA
-            3 => 5, // KETUA BEM
-            4 => 5, // KETUA BPM
-            5 => 4, // PEMBINA
-            6 => 8, // KETUA PRODI
-            7 => 3, // KETUA JURUSAN
-            8 => 2, // KOORDINATOR SUB BAGIAN
-            9 => 1  // WAKIL DIREKTUR
-        ];
-
-        $codeJabatan = $status_code_mapping[$status_flow] ?? null;
-
-        if ($codeJabatan !== null) {
-            $user = User::join('jabatan', 'users.jabatan_id', '=', 'jabatan.jabatan_id')
-                ->where('jabatan.code_jabatan', $codeJabatan)
-                ->when($status_flow == 2, function($query) use ($namaOrganisasi) {
-                    return $query->whereRaw('LOWER(users.organization) = ?', [strtolower($namaOrganisasi)]);
-                })
-                ->when($status_flow == 3, function($query) {
-                    return $query->whereRaw('LOWER(users.organization) LIKE ?', ['%bem%']);
-                })
-                ->when($status_flow == 4, function($query) {
-                    return $query->whereRaw('LOWER(users.organization) LIKE ?', ['%bpm%']);
-                })
-                ->select('users.email', 'users.name')
-                ->first();
-
-            return $user;
-        }
-        
-        return null;
-    }
-
+    
     private function sendNotificationEmail($user)
     {
         if ($user) {
